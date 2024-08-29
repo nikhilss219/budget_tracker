ebd3f3e6a2fc864912132af66fb369f2a04b6979
nikhilss219 <nikhil.wilsonk@gmail.com>
2024-08-29 17:35:16 +0530

    Added types

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import { Text, View}  from 'react-native'
 
 export default function Home(){
+
+    const [categories,setCategories] = useState();
+
     return (
         <View>
             <Text>Home Screen</Text>
