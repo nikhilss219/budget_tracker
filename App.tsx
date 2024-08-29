976c512ec18e57e91b277205662dfebcb4cd57ea
nikhilss219 <nikhil.wilsonk@gmail.com>
2024-08-29 17:24:13 +0530

    Added database connectivity

--- App.tsx
+++ App.tsx
@@ -1,12 +1,75 @@
+import * as React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system'
+import { Asset } from 'expo-asset';
+import { SQLiteProvider } from 'expo-sqlite/next'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+
+const Stack =createNativeStackNavigator();
+
+
+const loadDatabase = async () => {
+  const dbName ="mySQLiteDB.db";
+  const dbAsset =require("./assets/mySQLiteDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo =  await FileSystem.getInfoAsync(dbFilePath);
+  
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true}
+    );
+    await FileSystem.downloadAsync(dbUri,dbFilePath);
+  }
+}
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [dbLoaded,setdbLoaded] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    loadDatabase()
+      .then(()=>setdbLoaded(true))
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!dbLoaded) return(
+    <View style={{flex:1}}>
+      <ActivityIndicator size={"large"}/>
+      <Text>Loading Database</Text>
     </View>
+  )
+
+  return (
+    <NavigationContainer>
+      <React.Suspense
+      fallback={
+        <View style={{flex:1,backgroundColor:"red"}}>
+            <ActivityIndicator size={'large'}/>
+            <Text>Loading DB</Text>
+        </View>
+      }>
+        <SQLiteProvider 
+        databaseName='mySQLiteDB.db'
+        useSuspense>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home}
+            options={{
+              headerTitle:"Budget Tracker",
+              headerLargeTitle:true,
+            }}
+            />
+          </Stack.Navigator>
+        </SQLiteProvider>
+      </React.Suspense>
+      </NavigationContainer>
   );
 }
 
